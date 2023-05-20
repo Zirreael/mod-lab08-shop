1cf5dba02c2af35567a0db25300244c510d41fd7
Zirreael <126812503+Zirreael@users.noreply.github.com>
2023-05-20 14:48:29 +0300

    Update task.h

--- include/task.h
+++ include/task.h
@@ -11,83 +11,83 @@ using fsec = std::chrono::duration<float>;
 
 struct Statistic {
 public:
-	fsec workTime;
-	int acceptedClients;
-	int declinedClients;
-	std::vector<fsec> cashWorkTime;
-	fsec clientVisitTime;
-	int queueLength;
-	int queueTacts;
+  fsec workTime;
+  int acceptedClients;
+  int declinedClients;
+  std::vector<fsec> cashWorkTime;
+  fsec clientVisitTime;
+  int queueLength;
+  int queueTacts;
 
 public:
-	Statistic();
+  Statistic();
 };
 
 class Client {
 public:
-	int items;
-	Clock::time_point visitTime;
+  int items;
+  Clock::time_point visitTime;
 
 public:
-	explicit Client(int itemsCount);
+  explicit Client(int itemsCount);
 };
 
 class Shop {
 private:
-	class CashThread {
-	private:
-		std::thread thrd;
-		bool working;
-		int itemsCount;
-		std::mutex locMutex;
-		std::mutex& shopMutex;
-		Shop& shop;
+  class CashThread {
+  private:
+    std::thread thrd;
+    bool working;
+    int itemsCount;
+    std::mutex locMutex;
+    std::mutex& shopMutex;
+    Shop& shop;
 
-	public:
-		const int id;
-		const float intens;
+  public:
+    const int id;
+    const float intens;
 
-	private:
-		void Work();
-		void Release();
+  private:
+    void Work();
+    void Release();
 
-	public:
-		explicit CashThread(int ID, float intense, Shop& shop_, std::mutex& shopMutex_);
-		CashThread(const CashThread& other);
-		CashThread();
-		void Service(int items);
-		void Off();
+  public:
+    explicit CashThread(int ID, float intense, Shop& shop_, std::mutex& shopMutex_);
+    CashThread(const CashThread& other);
+    CashThread();
+    void Service(int items);
+    void Off();
 
-		~CashThread();
-	};
-	CashThread cash;
-	bool working;
-	std::thread pool;
-	std::queue<Client> clientsQueue;
-	std::vector<CashThread> cashThreads;
+    ~CashThread();
+  };
+  CashThread cash;
+  bool working;
+  std::thread pool;
+  std::queue<Client> clientsQueue;
+  std::vector<CashThread> cashThreads;
 
 public:
-	float intens;
-	int cashCount;
-	int maxLength;
-	std::mutex mtx;
-	std::queue<CashThread*> cashPool;
-	Statistic stats;
-	Clock::time_point startTime;
+  float intens;
+  int cashCount;
+  int maxLength;
+  std::mutex mtx;
+  std::queue<CashThread*> cashPool;
+  Statistic stats;
+  Clock::time_point startTime;
 
 private:
-	void CashControl();
+  void CashControl();
 
 public:
-	Shop(int count, float intensity, int maxLen);
-	Shop(const Shop& other);
-	void Service(Client client);
-	Statistic getStats();
-	CashThread& getCash(int ID);
-	bool IsWork();
-	void Off();
+  Shop(int count, float intensity, int maxLen);
+  Shop(const Shop& other);
+  void Service(Client client);
+  Statistic getStats();
+  CashThread& getCash(int ID);
+  bool IsWork();
+  void Off();
 
-	~Shop();
+  ~Shop();
 };
 
 void SpawnClients(Shop& shop, int count, float intens, int items);
