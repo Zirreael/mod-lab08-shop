4981557c8497c6133f07d9a1b3193971d245b170
Zirreael <126812503+Zirreael@users.noreply.github.com>
2023-05-20 14:43:27 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -6,10 +6,10 @@ TEST(TestCaseName, Test1) {
   float clientIntens = 30;
   int cash = 5;
   int items = 7;
-	int len = 10;
+  int len = 10;
   Shop shop(cash, shopIntens, len);
-	SpawnClients(shop, 100, clientIntens, items);
-	shop.Off();
+  SpawnClients(shop, 100, clientIntens, items);
+  shop.Off();
   auto stats = shop.getStats();
   int res = stats.acceptedClients + stats.declinedClients;
   EXPECT_EQ(res, 100);
@@ -17,39 +17,39 @@ TEST(TestCaseName, Test1) {
 
 TEST(TestCaseName, Test2) {
   float shopIntens = 10;
-	float clientIntens = 30;
-	int cash = 5;
-	int items = 7;
-	int len = 10;
+  float clientIntens = 30;
+  int cash = 5;
+  int items = 7;
+  int len = 10;
   Shop shop(cash, shopIntens, len);
-	SpawnClients(shop, 100, clientIntens, items);
-	shop.Off();
+  SpawnClients(shop, 100, clientIntens, items);
+  shop.Off();
   auto stats = shop.getStats();
   float workTime = stats.workTime.count();
   int id = 1;
-	float sumTime = 0;
-	for (auto& time : stats.cashWorkTime) {
-		sumTime += time.count();
-		id++;
-	}
-	float avgTime = sumTime / cash;
+  float sumTime = 0;
+  for (auto& time : stats.cashWorkTime) {
+    sumTime += time.count();
+    id++;
+  }
+  float avgTime = sumTime / cash;
   float res = workTime - avgTime;
   EXPECT_TRUE(res < avgTime);
 }
 
 TEST(TestCaseName, Test3) {
   float shopIntens = 10;
-	float clientIntens = 30;
-	int cash = 5;
+  float clientIntens = 30;
+  int cash = 5;
   int cash2 = 10;
-	int items = 7;
-	int len = 10;
+  int items = 7;
+  int len = 10;
   Shop shop(cash, shopIntens, len);
   Shop shop2(cash2, shopIntens, len);
-	SpawnClients(shop, 100, clientIntens, items);
-	shop.Off();
+  SpawnClients(shop, 100, clientIntens, items);
+  shop.Off();
   SpawnClients(shop2, 100, clientIntens, items);
-	shop2.Off();
+  shop2.Off();
   auto stats = shop.getStats();
   auto stats2 = shop2.getStats();
   int res = stats.acceptedClients;
@@ -60,16 +60,16 @@ TEST(TestCaseName, Test3) {
 TEST(TestCaseName, Test4) {
   float shopIntens = 10;
   float shopIntens2 = 5;
-	float clientIntens = 30;
-	int cash = 5;
-	int items = 7;
-	int len = 10;
+  float clientIntens = 30;
+  int cash = 5;
+  int items = 7;
+  int len = 10;
   Shop shop(cash, shopIntens, len);
   Shop shop2(cash, shopIntens2, len);
-	SpawnClients(shop, 100, clientIntens, items);
-	shop.Off();
+  SpawnClients(shop, 100, clientIntens, items);
+  shop.Off();
   SpawnClients(shop2, 100, clientIntens, items);
-	shop2.Off();
+  shop2.Off();
   auto stats = shop.getStats();
   auto stats2 = shop2.getStats();
   int res = stats.acceptedClients;
